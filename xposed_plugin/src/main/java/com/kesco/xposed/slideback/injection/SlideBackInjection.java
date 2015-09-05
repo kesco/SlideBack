0f98edb80b4f027a5de0cd7e346e5f1a836d40ce
Kesco Lin <kesco915@gmail.com>
2015-09-05 23:44:32 +0800

    Added: 加入Xposed测试

---
+++ xposed_plugin/src/main/java/com/kesco/xposed/slideback/injection/SlideBackInjection.java
@@ -0,0 +1,51 @@
+package com.kesco.xposed.slideback.injection;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by kesco on 15-9-4.
+ */
+public class SlideBackInjection implements IXposedHookLoadPackage, IXposedHookZygoteInit {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.kesco.demo.imsi")) {
+            return;
+        }
+        XposedBridge.log(lpparam.packageName + " detected!");
+        XposedHelpers.findAndHookMethod(Activity.class, "setContentView", int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("before setContentView");
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("after setContentView");
+            }
+        });
+        XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("before onCreate");
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                XposedBridge.log("after onCreate");
+            }
+        });
+    }
+
+    @Override
+    public void initZygote(StartupParam suparma) throws Throwable {
+        XposedBridge.log(suparma.modulePath);
+    }
+}
