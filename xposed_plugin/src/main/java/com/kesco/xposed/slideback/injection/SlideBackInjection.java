ab5c36d001d640938d07b0391ae79e0c92e9d24f
Kesco Lin <kesco915@gmail.com>
2015-09-07 17:56:24 +0800

    Added: 加入侧滑库

--- xposed_plugin/src/main/java/com/kesco/xposed/slideback/injection/SlideBackInjection.java
+++ xposed_plugin/src/main/java/com/kesco/xposed/slideback/injection/SlideBackInjection.java
@@ -1,7 +1,21 @@
 package com.kesco.xposed.slideback.injection;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import com.kesco.adk.moko.slideback.SlideEdge;
+import com.kesco.adk.moko.slideback.SlideLayout;
+import com.kesco.adk.moko.slideback.SlideListener;
+import com.kesco.adk.moko.slideback.SlideState;
+import com.kesco.adk.moko.slideback.SlidebackPackage;
+
+import org.jetbrains.annotations.NotNull;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
@@ -10,47 +24,53 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
-/**
- * Created by kesco on 15-9-4.
- */
 public class SlideBackInjection implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         if (!lpparam.packageName.equals("com.kesco.demo.imsi")) {
             return;
         }
-        XposedBridge.log(lpparam.packageName + " detected!");
         XposedHelpers.findAndHookMethod("android.support.v7.app.AppCompatActivity", lpparam.classLoader, "setContentView", "int", new XC_MethodHook() {
             @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("before AppCompatActivity setContentView");
-            }
-
-            @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("after AppCompatActivity setContentView");
+                final Activity act = (Activity) param.thisObject;
+                Window win = act.getWindow();
+                ViewGroup decorView = (ViewGroup) win.getDecorView();
+                Drawable bg = decorView.getBackground();
+                act.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                decorView.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                View screenView = decorView.getChildAt(0);
+                decorView.removeViewAt(0);
+                SlideLayout slideLayout = new SlideLayout(act, screenView);
+                slideLayout.addView(screenView);
+                decorView.addView(slideLayout, 0);
+                screenView.setBackgroundDrawable(bg);
+                slideLayout.setSlideEdge(SlideEdge.LEFT);
+                slideLayout.setListener(new SlideListener() {
+                    @Override
+                    public void onSlideStart() {
+                    }
+
+                    @Override
+                    public void onSlide(float percent, @NotNull SlideState state) {
+                        XposedBridge.log(act.getClass().getSimpleName() + ": " + percent);
+                    }
+
+                    @Override
+                    public void onSlideFinish() {
+                        XposedBridge.log(act.getClass().getSimpleName() + " : Finish");
+                        act.finish();
+                        act.overridePendingTransition(0, 0);
+                    }
+                });
+                SlidebackPackage.convertActivityFromTranslucent(act);
             }
         });
-        XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "setContentView", "int", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("before setContentView");
-            }
-
+        XposedHelpers.findAndHookMethod("android.support.v7.app.AppCompatActivity", lpparam.classLoader, "onPostCreate", Bundle.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("after setContentView");
-            }
-        });
-        XposedHelpers.findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("before onCreate");
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                XposedBridge.log("after onCreate");
+                Activity act = (Activity) param.thisObject;
+                SlidebackPackage.convertActivityToTranslucent(act);
             }
         });
     }
